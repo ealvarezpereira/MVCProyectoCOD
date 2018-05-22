869c93c43e00b3219e465ac8306a023f8ba48d87
quique <quique@quique-Aspire-E5-575G>
2018-05-22 13:16:44 +0200

    Añadido primary key.

--- src/prgproyectobd/CrearTabla.java
+++ src/prgproyectobd/CrearTabla.java
@@ -8,6 +8,7 @@ package prgproyectobd;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -45,7 +46,6 @@ public class CrearTabla extends javax.swing.JFrame {
         unique = new javax.swing.JCheckBox();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        iniciarcreacion = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -72,12 +72,6 @@ public class CrearTabla extends javax.swing.JFrame {
             }
         });
 
-        nomcampo.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                nomcampoMouseClicked(evt);
-            }
-        });
-
         tipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "INTEGER", "TEXT" }));
 
         notnull.setText("NOT NULL");
@@ -89,13 +83,6 @@ public class CrearTabla extends javax.swing.JFrame {
         jLabel2.setText("Nombre campo");
 
         jLabel3.setText("Tipo");
-
-        iniciarcreacion.setText(".");
-        iniciarcreacion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                iniciarcreacionActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -111,8 +98,6 @@ public class CrearTabla extends javax.swing.JFrame {
                                     .addComponent(jLabel1)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(nomtabla, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(iniciarcreacion, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGap(0, 0, Short.MAX_VALUE))
                                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -143,11 +128,10 @@ public class CrearTabla extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(19, 19, 19)
+                .addGap(23, 23, 23)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(nomtabla, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(iniciarcreacion))
+                    .addComponent(nomtabla, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(addfield)
@@ -163,7 +147,7 @@ public class CrearTabla extends javax.swing.JFrame {
                 .addComponent(primarykey)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(unique)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(aceptar)
                     .addComponent(cancelar))
@@ -173,8 +157,31 @@ public class CrearTabla extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    ArrayList<String> nombres = new ArrayList<String>();
+    ArrayList<String> nprimary = new ArrayList<String>();
+    boolean añadido = false;
+    boolean seleccionado = false;
     int m = 0;
+
     private void addfieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addfieldActionPerformed
+
+        if (m == 0) {
+            sqlcreate = "CREATE TABLE " + nomtabla.getText() + "(";
+            nombres.add(nomcampo.getText());
+        } else {
+            for (int i = 0; i < nombres.size(); i++) {
+                if (nomcampo.getText().equals(nombres.get(i))) {
+                    añadido = false;
+                    nomcampo.setText(JOptionPane.showInputDialog("Ese nombre está repetido."));
+                } else {
+                    añadido = true;
+                }
+            }
+        }
+
+        if (añadido == true) {
+            nombres.add(nomcampo.getText());
+        }
 
         String sentencia = "";
         sentencia = String.valueOf(tipo.getSelectedItem() + " ");
@@ -185,6 +192,10 @@ public class CrearTabla extends javax.swing.JFrame {
         if (unique.isSelected()) {
             sentencia = sentencia + unique.getText() + " ";
         }
+        if (primarykey.isSelected()) {
+            seleccionado = true;
+            nprimary.add(nomcampo.getText());
+        }
 
         if (m == 0) {
             sqlcreate = sqlcreate + nomcampo.getText() + " " + sentencia;
@@ -193,9 +204,11 @@ public class CrearTabla extends javax.swing.JFrame {
         }
 
         m++;
+        añadido = false;
         System.out.println(sqlcreate);
         nomtabla.setText(null);
         nomcampo.setText(null);
+        primarykey.setSelected(false);
         notnull.setSelected(false);
         unique.setSelected(false);
     }//GEN-LAST:event_addfieldActionPerformed
@@ -213,7 +226,24 @@ public class CrearTabla extends javax.swing.JFrame {
 
     private void aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aceptarActionPerformed
 
+        if (seleccionado == true) {
+            sqlcreate = sqlcreate + "," + primarykey.getText() + "(";
+            System.out.println(sqlcreate);
+            for (int i = 0; i < nprimary.size(); i++) {
+                if (i == 0) {
+                    sqlcreate = sqlcreate + nprimary.get(i);
+                    System.out.println(sqlcreate);
+                } else {
+                    sqlcreate = sqlcreate +","+ nprimary.get(i);
+                    System.out.println(sqlcreate);
+                }
+            }
+            sqlcreate = sqlcreate + ")";
+            System.out.println(sqlcreate);
+        }
+        
         sqlcreate = sqlcreate + ");";
+        System.out.println(sqlcreate);
 
         try {
             PreparedStatement st = PRGProyectoBD.conn.prepareStatement(sqlcreate);
@@ -226,14 +256,6 @@ public class CrearTabla extends javax.swing.JFrame {
             System.out.println("Error " + ex);
         }
     }//GEN-LAST:event_aceptarActionPerformed
-
-    private void nomcampoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_nomcampoMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_nomcampoMouseClicked
-
-    private void iniciarcreacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_iniciarcreacionActionPerformed
-        sqlcreate = "CREATE TABLE " + nomtabla.getText() + "(";
-    }//GEN-LAST:event_iniciarcreacionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -275,7 +297,6 @@ public class CrearTabla extends javax.swing.JFrame {
     public javax.swing.JButton aceptar;
     public javax.swing.JButton addfield;
     public javax.swing.JButton cancelar;
-    public javax.swing.JButton iniciarcreacion;
     public javax.swing.JLabel jLabel1;
     public javax.swing.JLabel jLabel2;
     public javax.swing.JLabel jLabel3;
