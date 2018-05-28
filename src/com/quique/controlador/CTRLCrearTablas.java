11de61de215aabbe26de3d1253ee32ad1b65faed
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-05-28 12:47:50 +0200

    Añadidos metodos de control.

--- src/com/quique/controlador/CTRLCrearTablas.java
+++ src/com/quique/controlador/CTRLCrearTablas.java
@@ -5,10 +5,63 @@
  */
 package com.quique.controlador;
 
+import com.quique.modelo.CNXCrearTablas;
+import com.quique.vista.VISTACrearTablas;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+
 /**
  *
  * @author quique
  */
 public class CTRLCrearTablas {
+
+    private static String nomtabla;
+
+    public static String retNomTabla() {
+        return nomtabla = VISTACrearTablas.nomtabla.getText();
+    }
+
+    private static String nomcampo;
+
+    public static String retNomCampo() {
+        return nomcampo = VISTACrearTablas.nomcampo.getText();
+    }
+
+    public static void setNomcampo(String nomcampo) {
+        CTRLCrearTablas.nomcampo = nomcampo;
+    }
+
+    private static JComboBox<String> tipo;
+
+    public static JComboBox retTipo() {
+        return tipo = VISTACrearTablas.tipo;
+    }
+
+    private static JCheckBox unique;
+
+    public static JCheckBox retUnique() {
+        return unique = VISTACrearTablas.unique;
+    }
+
+    private static JCheckBox notnull;
+
+    public static JCheckBox retNotnull() {
+        return notnull = VISTACrearTablas.notnull;
+    }
+
+    private static JCheckBox primarykey;
+
+    public static JCheckBox retPrmKey() {
+        return primarykey = VISTACrearTablas.primarykey;
+    }
     
+    public static void añadirCampo(){
+        CNXCrearTablas.añadirCampo();
+    }
+    
+    public static void botAceptar(){
+    
+        CNXCrearTablas.btnAceptar();
+    }
 }
