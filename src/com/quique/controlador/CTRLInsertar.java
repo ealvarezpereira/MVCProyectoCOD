f66412bd22c9fb22d32be2e1e8db262a3aab0fd0
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-05-29 08:37:52 +0200

    Añadidos metodos de control

--- src/com/quique/controlador/CTRLInsertar.java
+++ src/com/quique/controlador/CTRLInsertar.java
@@ -5,10 +5,42 @@
  */
 package com.quique.controlador;
 
+import com.quique.modelo.CNXInsertar;
+import com.quique.vista.VISTAInsertar;
+import java.sql.ResultSet;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author quique
  */
 public class CTRLInsertar {
+
+    private static ResultSet r;
+
+    public static ResultSet resul() {
+        return CNXInsertar.constructorInsertar();
+    }
+
+    private static JTable tab;
+
+    public static JTable datos() {
+        return tab = VISTAInsertar.datos;
+    }
+
+    private static JComboBox<String> tablas;
+
+    public static JComboBox tablas() {
+        return tablas = VISTAInsertar.tablas;
+    }
+
+    public static DefaultTableModel recargar() {
+        return CNXInsertar.recargar();
+    }
     
+    public static DefaultTableModel añadir(){
+        return CNXInsertar.añadir();
+    }
 }
